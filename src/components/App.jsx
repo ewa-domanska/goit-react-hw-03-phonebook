cfb845bece8528612e4987ff999a93202bd62d65
Ewa Domańska <ewadomanska0510@gmail.com>
2022-09-19 19:02:02 +0200

    Add localstorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,15 +6,20 @@ import {ContactList} from "./ContactList/ContactList";
 
 export class App extends Component {
   state = {
-    contacts: [
-      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-    ],
+    contacts: [],
     filter: '',
     name: '',
     number: ''
+  }
+
+  componentDidMount = () => {
+    this.setState({
+      contacts: JSON.parse(localStorage.getItem("contacts"))
+    })
+  }
+
+  componentDidUpdate = () => {
+    localStorage.setItem("contacts", JSON.stringify(this.state.contacts))
   }
 
   createContact = (name, number) => {
